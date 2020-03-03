1f527aad5960f848ad563508a5f2840f17a9838b
pablolucianop <pablolucianop@gmail.com>
2020-03-03 02:18:14 -0300

    fixed doble action

--- trjs.js
+++ trjs.js
@@ -14,12 +14,13 @@ var unsplittables = ['br','cr','dr', 'gr', 'fr', 'kr', 'tr','bl', 'cl', 'gl', 'f
 ////BACKGROUND COLOUR MANAGEMENT
 var backgroundColorStage=0
 var backgroundColours = ['#a7d184', '#9bc6c3', '#edde9c']
+var backgroundColoursShadows = ['#ddce92', '#8eb5b2', '#99c079']
 
 changeBackground= () =>{
 	backgroundColorStage = backgroundColorStage + 1
-	if (backgroundColorStage > 0 ) {
 		document.body.style.backgroundColor = backgroundColours[backgroundColorStage % 3];
-	}
+		// ss
+	
 }
 
 
@@ -352,15 +353,21 @@ showSyllables=() =>{
 	var input = document.getElementById("userInput").value;
 	var inputWord = cutAWordInSylables(input)
 	var presentedWord = []
+	console.log(presentedWord)
 	showNextSyllable=()=>{
-		// changeBackground()
-		presentedWord.push(inputWord[presentedWord.length ] )
-	  	document.getElementById("texto").innerHTML = presentedWord.join(" - ");  
-  	}
-	inputWord.forEach(element => {
-		showNextSyllable()
-	});
+		presentedWord.push(inputWord[presentedWord.length])
+		document.getElementById("texto").innerHTML = presentedWord.join(" - ");  
+	  }
 	
+	for (let index = 0; index < inputWord.length; index++) {
+		
+		setTimeout(function () { showNextSyllable()
+			changeBackground()
+		}, 0 + (1000 * index));	
+	}
+		
+
+
  }
 
 
